e68065af017fa7437387d73d16b578cbd40273bf
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-08-12 15:59:07 -0500

    Added context tags with values

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//contexts
+import {ProductContext} from './contexts/ProductContext.js'
+import {CartContext} from './contexts/CartContext.js'
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -16,6 +20,8 @@ function App() {
 	};
 
 	return (
+		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value={cart}>
 		<div className="App">
 			<Navigation cart={cart} />
 
@@ -35,7 +41,10 @@ function App() {
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			
 		</div>
+		</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
